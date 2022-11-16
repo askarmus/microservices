bfe1e491ea4769ec5f49b47421c69f50d83c57b5
Askar Musthaffa <askarmus@hotmail.com>
2022-11-16 09:10:28 +0530

    Initial commit

--- BlazorServerWebUI/Program.cs
+++ BlazorServerWebUI/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://apigateway") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:51258") });
 
 var app = builder.Build();
 
